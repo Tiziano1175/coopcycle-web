3f7383adc451903d42199d018108b2f7aa670424
TF-gmail <f.tiziano@gmail.com>
2019-12-28 19:49:38 +0100

    gestione categorie in admin

---
+++ src/AppBundle/Entity/RestaurantCategory.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Gedmo\Timestampable\Traits\Timestampable;
+
+class RestaurantCategory
+{
+    use Timestampable;
+
+    private $id;
+
+    protected $name;
+
+    protected $description;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+
+}
